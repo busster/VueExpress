530ed5213b25319fa3ece64674223c3eb543b880
busster <jasonmbuss@gmail.com>
2017-12-12 10:43:03 -0600

    first commit

---
+++ node/app.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+app.use(express.static('public/dist'))
+app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/public/dist/index.html')))
+
+app.listen(3000)
